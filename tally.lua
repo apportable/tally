2928a596f19af9e8c28813508818a8337c85b0d8
Craig Barnes <cr@igbarn.es>
2012-06-03 02:55:25 +0100

    Add file type detection to tally.lua

--- tally.lua
+++ tally.lua
@@ -2,7 +2,7 @@
 
 local lfs = require "lfs"
 
-function dirtree(dir)
+local function dirtree(dir)
     assert(dir and dir ~= "", "directory parameter is missing or empty")
     if dir:sub(-1) == "/" then dir = dir:sub(1, -2) end
     local function yieldtree(dir)
@@ -21,6 +21,44 @@ function dirtree(dir)
     return coroutine.wrap(function() yieldtree(dir) end)
 end
 
+local function firstline(filename)
+    local file = io.open(filename, "r")
+    if file then
+        local hashbang = file:read()
+        file:close()
+        return hashbang
+    end
+end
+
+local extensions = {
+    c = "C",
+    lua = "Lua",
+    rb = "Ruby",
+}
+
+local hashbangs = {
+    Lua = "^#!.*/bash",
+    AWK = "^#!.*/awk",
+    SED = "^#!.*/sed",
+}
+
+local function findtype(filename)
+    local ext = filename:match("^.*%.(.*)$")
+    if ext and extensions[ext:lower()] then
+        return extensions[ext:lower()]
+    else
+        local hashbang = firstline(filename)
+        if hashbang and hashbang:sub(1, 2) == "#!" then
+            for k, v in pairs(hashbangs) do
+                if hashbang:find(v) then
+                    return k
+                end
+            end
+        end
+    end
+end
+
 for filename in dirtree(os.getenv("PWD")) do
-    print(filename)
+    local filetype = findtype(filename)
+    if filetype then print(filetype, filename) end
 end

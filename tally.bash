58f5cd66bff9f57db335b00ba2a8d6ce729d6a7e
Craig Barnes <cr@igbarn.es>
2013-09-23 15:17:28 +0100

    Replace non-portable regex with strict POSIX BRE in tally.bash

--- tally.bash
+++ tally.bash
@@ -6,32 +6,36 @@ IFS=$'\n' # Prevent filenames with whitespace from being split
 export POSIXLY_CORRECT=1
 declare -A SUB
 
+blank='/^[[:space:]]*$/d'
+hash='/^[[:space:]]*#/d'
+ruby='/^=begin$/,/^=end$/d'
+
 stripc() {
-    awk -v RS='\\*\\/' '{gsub(/\/\*.*/,"")}1' $1 | sed '/^[:space:]*$/d'
+    awk -v RS='\\*\\/' '{gsub(/\/\*.*/,"")}1' $1 | sed "$blank"
 }
 
 stripxml() {
-    awk -v RS='-->' '{gsub(/<!--.*/,"")}1' $1 | sed '/^\s*$/d'
+    awk -v RS='-->' '{gsub(/<!--.*/,"")}1' $1 | sed "$blank"
 }
 
 count() {
     case $2 in
     AWK|CoffeeScript|Make|Perl|Python|Sed|Shell|TCL|YAML)
-        let SUB[$2]+=$(sed -r '/^\s*(#|$)/d' $1 | wc -l);;
+        let SUB[$2]+=$(sed "$blank;$hash" $1 | wc -l);;
     Ruby)
-        let SUB[$2]+=$(sed -r '/^\s*(#|$)/d;/^=begin$/,/^=end$/d' $1 | wc -l);;
+        let SUB[$2]+=$(sed "$blank;$hash;$ruby" $1 | wc -l);;
     C|CSS)
         let SUB[$2]+=$(stripc $1 | wc -l);;
     C++|Go|JavaScript|Vala|SCSS|Java)
-        let SUB[$2]+=$(stripc $1 | sed '/^\s*\/\//d' | wc -l);;
+        let SUB[$2]+=$(stripc $1 | sed '/^[[:space:]]*\/\//d' | wc -l);;
     SQL|Lua)
-        let SUB[$2]+=$(sed -r '/^\s*(\-\-|$)/d' $1 | wc -l);;
+        let SUB[$2]+=$(sed "$blank;/^[[:space:]]*\-\-/d" $1 | wc -l);;
     HTML|Mallard|XML)
         let SUB[$2]+=$(stripxml $1 | wc -l);;
     JSON|Markdown)
-        let SUB[$2]+=$(sed '/^\s*$/d' $1 | wc -l);;
+        let SUB[$2]+=$(sed "$blank" $1 | wc -l);;
     Scheme)
-        let SUB[$2]+=$(sed -r '/^\s*($|;)/d' $1 | wc -l);;
+        let SUB[$2]+=$(sed "$blank;/^[[:space:]]*;/d" $1 | wc -l);;
     esac
 }
 
@@ -71,7 +75,7 @@ for F in $(find $@ -type f -not -path "*/.*"); do
         *.xml|*.ui|*.glade) count $F XML;;
         *[Mm]akefile|*.mk)  count $F Make;;
         # If no filename pattern matches, try to match hashbang:
-        *) case "$(sed -rn '1s|(\#!.*/(env)?\s*)?(.*)|\3|p' $F)" in
+        *) case "$(sed -n '1s|^[^[:space:]]*/\([a-z]*\).*|\1|p' $F)" in
             awk*|gawk*)     count $F AWK;;
             bash|sh)        count $F Shell;;
             coffee*)        count $F CoffeeScript;;
@@ -81,10 +85,6 @@ for F in $(find $@ -type f -not -path "*/.*"); do
             python*)        count $F Python;;
             ruby*)          count $F Ruby;;
             sed*)           count $F Sed;;
-            *html*)         count $F HTML;;
-            *mallard*)      count $F Mallard;;
-            *xml*)          count $F XML;;
-            %YAML*)         count $F YAML;;
         esac;;
     esac
 done

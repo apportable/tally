2a786f317347c0499f7e00ae35d54d25f9b0cafd
Craig Barnes <cr@igbarn.es>
2013-12-15 21:01:52 +0000

    Update license header in tally.lua

--- tally.lua
+++ tally.lua
@@ -1,6 +1,17 @@
 #!/usr/bin/lua
--- Copyright 2012-2013 Craig Barnes
--- Licensed under the ISC license
+--[[
+ Tally: a simple, command-line SLOC counter.
+ Copyright (C) 2012-2013 Craig Barnes
+
+ This program is free software; you can redistribute it and/or modify it
+ under the terms of the GNU General Public License version 3, as published
+ by the Free Software Foundation.
+
+ This program is distributed in the hope that it will be useful, but
+ WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General
+ Public License version 3 for more details.
+]]
 
 local lfs = require "lfs"
 local lpeg = require "lpeg"
